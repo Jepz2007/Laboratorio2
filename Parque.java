f183f028d294a115c89d865fbbbf1df103f6d1d4
Justin Paz <paz26879@uvg.edu.gt>
2026-09-05 18:11:23 -0600

    Creación de constructor, menú y array en parque

--- Parque.java
+++ Parque.java
@@ -1,4 +1,18 @@
 package Lab2.Laboratorio2;
 public class Parque {
+    private int idParque;
+    private String nombreParque;
+    private String nombreEncargado;
+    private PuntoAcceso[] puntosAcceso;
+
+    public Parque(int idParque, String nombreParque, String nombreEncargado){
+        this.idParque = idParque;
+        this.nombreParque = nombreParque;
+        this.nombreEncargado = nombreEncargado;
+        this.puntosAcceso = new PuntoAcceso[5];
+    }
+    
+
+
     
 }
